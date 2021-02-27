676378f22f14c81d5d0ca774fd2adc184deba813
ilopezr <i.lopezr.2020@alumnos.urjc.es>
2021-02-27 14:18:00 +0100

    .

--- P0/Ex7.py
+++ P0/Ex7.py
@@ -4,9 +4,7 @@ from pathlib import Path
 GENE_FOLDER = './sequences/'
 FILENAME = 'U5.txt'
 
-new_fragment = Seq0.seq_read_fasta(GENE_FOLDER + FILENAME)
-new_fragment = new_fragment[0:20]
-print(new_fragment)
+new_fragment = Seq0.seq_read_fasta(GENE_FOLDER + FILENAME)[0:20]
 
 print('--------- | Exercise 7 | ---------')
 print('Gene U5: ')
