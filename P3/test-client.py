c631aee53ecf97736f99f55ff2d69f0379473152
ilopezr <i.lopezr.2020@alumnos.urjc.es>
2021-04-16 19:32:57 +0200

    .

--- P3/test-client.py
+++ P3/test-client.py
@@ -6,24 +6,32 @@ IP = "localhost"
 
 client = Client(IP, PORT) #Client creado
 
-msg = input('Enter here the command you want to test: ')
+correct = True
+while correct:
+    msg = input('Enter here the command you want to test: ')
 
-if msg == 'PING':
-    client.talk(msg)
+    if msg == 'EXIT':
+        print('The process has finished correctly.')
+        correct = False
 
-elif msg == 'GET':
-    for i in range(0,5):
-        client.talk(msg + ' ' + str(i))
+    elif msg == 'PING':
+        client.talk(msg)
 
-elif msg == 'INFO' or msg == 'COMP' or msg == 'REV':
-    msg = msg + ' ' + str('AAACCCTTTT')  #client.talk('GET 0')
-    client.talk(msg)
+    elif msg == 'GET':
+        for i in range(0,5):
+            client.talk(msg + ' ' + str(i))
 
-elif msg == 'GENE':
-    list_gene_names = ['U5', 'ADA', 'FRAT1', 'FXN' , 'RNU6_269P']
-    for i in list_gene_names:
-        client.talk(msg + ' ' + str(i))
+    elif msg == 'INFO' or msg == 'COMP' or msg == 'REV':
+        msg = msg + ' ' + str('AAACCCTTTT')  #client.talk('GET 0')
+        client.talk(msg)
 
-else:
-    print('The command is not correct. Try with this ones: PING, GET, INFO, COMP, REV, GENE')
+    elif msg == 'GENE':
+        list_gene_names = ['U5', 'ADA', 'FRAT1', 'FXN' , 'RNU6_269P']
+        for i in list_gene_names:
+            client.talk(msg + ' ' + str(i))
+
+    else:
+        print('The command is not correct. Try with this ones: PING, GET, INFO, COMP, REV, GENE')
+
+
 
